d45c068da94a84e71db93e06b042876636a601af
HaMeiAltVergessen <sebastian-raabe@hotmail.de>
2025-11-19 10:46:16 +0100

    Initial Commit
    Project SetUP

---
+++ scripts/GameManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var score: int = 0
+var coins: int = 0
+var highscores: Array = []
+var active_items: Array = []
+var items: Dictionary = {}
+var total_score: int = 0
+var unlocked_levels: int = 1
+var highest_combos: Array = []
